8324cbb852042d34d72b3d4d309f325b85a2a18b
td1311 <41880710+td1311@users.noreply.github.com>
2023-04-12 23:17:48 +0700

    update scrollview

--- test_scrollview/ViewController.swift
+++ test_scrollview/ViewController.swift
@@ -17,17 +17,17 @@ class ViewController: UIViewController {
 
 }
 
-extension ViewController: UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 40
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = "Hello \(indexPath)"
-        return cell
-    }
-}
+//extension ViewController: UITableViewDelegate, UITableViewDataSource {
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return 40
+//    }
+//    
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+//        cell.textLabel?.text = "Hello \(indexPath)"
+//        return cell
+//    }
+//}
 
 extension ViewController: UIScrollViewDelegate {
     
